0319e75eb691aa76a1e72b9303c0e9249c7a3af1
xieminsen <1558541642@qq.com>
2019-06-19 10:18:12 +0800

    新添加了一些实体类

--- house-common/src/main/java/com/xms/house/entity/User.java
+++ house-common/src/main/java/com/xms/house/entity/User.java
@@ -1,131 +1,71 @@
 package com.xms.house.entity;
 
+
 import java.util.Date;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import com.xms.house.util.BaseEntity;
-
-/**
- * 实现序列化
- * @author xie
- *
- */
-public class User extends BaseEntity{
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+public class User {
 
 	private Long id;
+	
+	private String email;
+	
+	private String phone;
+	
+	private String name;
+	
+	private String passwd;
+	
+	private String confirmPasswd;
+	
+	private Integer type;//普通用户1，经纪人2
+	
+	private Date   createTime;
+	
+	private Integer enable;
+	
+	private String  avatar;
+	
+	private MultipartFile avatarFile;
+	
+	private String newPassword;
+	
+	private String key;
+	
+	private Long   agencyId;
+	
+	private String aboutme;
+	
+	private String agencyName;
 
-    private String name;
-
-    private String phone;
-
-    private String email;
-
-    private String aboutme;
-
-    private String passwd;
-
-    private String avatar;
-
-    private int type;
-
-    private Date createTime;
-
-    private int enable;
-
-    private Integer agencyId;
-    
-    private String confirmPasswd;
-    
-    private MultipartFile avatarFile;
-    
-    private String agencyName;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone == null ? null : phone.trim();
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email == null ? null : email.trim();
-    }
-
-    public String getAboutme() {
-        return aboutme;
-    }
-
-    public void setAboutme(String aboutme) {
-        this.aboutme = aboutme == null ? null : aboutme.trim();
-    }
-
-    public String getPasswd() {
-        return passwd;
-    }
-
-    public void setPasswd(String passwd) {
-        this.passwd = passwd == null ? null : passwd.trim();
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar == null ? null : avatar.trim();
-    }
-
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-
-    public int getEnable() {
-		return enable;
+	public Long getId() {
+		return id;
 	}
 
-	public void setEnable(int enable) {
-		this.enable = enable;
+	public String getEmail() {
+		return email;
 	}
 
-	public Integer getAgencyId() {
-        return agencyId;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setAgencyId(Integer agencyId) {
-        this.agencyId = agencyId;
-    }
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
 
 	public String getConfirmPasswd() {
 		return confirmPasswd;
@@ -133,6 +73,47 @@ public class User extends BaseEntity{
 
 	public void setConfirmPasswd(String confirmPasswd) {
 		this.confirmPasswd = confirmPasswd;
+	}
+
+	public String getAboutme() {
+		return aboutme;
+	}
+
+	public void setAboutme(String aboutme) {
+		this.aboutme = aboutme;
+	}
+
+	public Integer getType() {
+		return type;
+	}
+
+	public void setType(Integer type) {
+		this.type = type;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Integer getEnable() {
+		return enable;
+	}
+
+	public void setEnable(Integer enable) {
+		this.enable = enable;
+	}
+
+
+	public String getNewPassword() {
+		return newPassword;
+	}
+
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
 	}
 
 	public MultipartFile getAvatarFile() {
@@ -144,24 +125,48 @@ public class User extends BaseEntity{
 	}
 
 	public String getAgencyName() {
-		return agencyName;
+    return agencyName;
+  }
+
+  public void setAgencyName(String agencyName) {
+    this.agencyName = agencyName;
+  }
+
+  public String getKey() {
+		return key;
 	}
 
-	public void setAgencyName(String agencyName) {
-		this.agencyName = agencyName;
+	public void setKey(String key) {
+		this.key = key;
 	}
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	public Long getAgencyId() {
+		return agencyId;
 	}
 
-	public int getType() {
-		return type;
+	public void setAgencyId(Long agencyId) {
+		this.agencyId = agencyId;
 	}
 
-	public void setType(int type) {
-		this.type = type;
+	public String getAvatar() {
+		return avatar;
 	}
-    
-    
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
 }
